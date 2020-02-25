c0db02aa51e7b71294ac616eedc70d2f1a127f80
Bernie Reiter <ockham@raz.or.at>
2020-02-25 22:05:17 +0600

    Properly proxy requests

--- packages/data-stores/src/index.ts
+++ packages/data-stores/src/index.ts
@@ -4,10 +4,11 @@
 import * as User from './user';
 import * as DomainSuggestions from './domain-suggestions';
 import * as Site from './site';
+import * as Utils from './utils';
 import * as Verticals from './verticals';
 import * as VerticalsTemplates from './verticals-templates';
 
-export { User, DomainSuggestions, Site, Verticals, VerticalsTemplates };
+export { User, DomainSuggestions, Site, Utils, Verticals, VerticalsTemplates };
 
 /**
  * Helper types
